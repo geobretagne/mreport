9d3d59d70097cbe342ed17f076e36ec66fbb3f29
spelhate <sebastien.pelhate@bretagne.bzh>
2019-09-05 11:36:35 +0200

    Update reports

--- reports/gare/data.php
+++ reports/gare/data.php
@@ -1,32 +1,30 @@
-<?php
+﻿<?php
    include("/var/php-secure/db-settings.php");
    header('Content-Type: application/json');
-
-
+   
     $dataid = $_GET['dataid'];
-
     $params = array($dataid);
 
     $db = pg_connect($db_territoires);
-
     unset ($db_territoires);
      if (!$db) {
          echo json_encode(array("Connexion à la Base Impossible avec les paramètres fournis"));
          die();
      }
 
-    $sql = "SELECT * FROM indicateur._all WHERE dataid = $1 order by dataviz,dataset,\"order\"";
-
-    if (empty($_GET['dataid'])) {
-       $sql = "SELECT distinct dataid FROM indicateur._all";
+     $sql = "SELECT * FROM indicateur.gare WHERE dataid = $1 order by dataviz,dataset,\"order\"";
+     
+     if (empty($_GET['dataid'])) {
+       $sql = "SELECT distinct dataid FROM indicateur.gare";
        $params = [];
     }
 
 
 
-    $ps = pg_prepare($db, "mreport_territoires", $sql);
 
-    $query = pg_execute($db, "mreport_territoires", $params);
+    $ps = pg_prepare($db, "mreport_gare", $sql);
+
+    $query = pg_execute($db, "mreport_gare", $params);
 
     if (!$query ) {
         echo  json_encode(pg_last_error($db));
@@ -36,7 +34,7 @@
     // free memory
     pg_free_result($query);
     // close connection
-     if ($db) {
+     if (!$db) {
         pg_close($db);
      }
 
