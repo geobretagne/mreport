666168930c0c18a272f8bc0325426280544bfa4d
Sfoucheur <sebastien.foucheur@bretagne.bzh>
2020-06-22 17:28:48 +0200

    Fix #36

--- frontend/static/js/textConfiguration.js
+++ frontend/static/js/textConfiguration.js
@@ -77,9 +77,7 @@ textedit = (function () {
         _applyTextStyle(modalText,editedStyle);
 
         // Create color picker for the Modal
-        var pk = new Piklor(".text-color-picker", [
-                "#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e", "#16a085", "#27ae60", "#2980b9", "#8e44ad", "#2c3e50", "#f1c40f", "#e67e22", "#e74c3c", "#ecf0f1", "#95a5a6", "#f39c12", "#d35400", "#c0392b", "#bdc3c7", "#7f8c8d"
-            ], {
+        var pk = new Piklor(".text-color-picker",composer.activeModel().parameters.colors, {
                 open: ".textcolor-config-wrapper .btn"
             }),
             input = pk.getElm(".textcolor-config-wrapper .input");
--- frontend/static/js/wizard.js
+++ frontend/static/js/wizard.js
@@ -615,32 +615,7 @@ wizard = (function () {
             $("#picker-wrapper").append('<div class="colorbtn colorbtn' + colorbtn + '"></div>');
             $(".colorbtn" + colorbtn).css('background-color', saved.color ? saved.color : "#FFF");
             $(".chosecolors").append('<div class="available_colors color-picker' + colorbtn + '"></div>');
-            var pk = new Piklor(".color-picker" + colorbtn, [
-                "#1abc9c",
-                "#2ecc71",
-                "#3498db",
-                "#9b59b6",
-                "#34495e",
-                "#16a085",
-                "#27ae60",
-                "#2980b9",
-                "#8e44ad",
-                "#2c3e50",
-                "#f1c40f",
-                "#e67e22",
-                "#e74c3c",
-                "#ecf0f1",
-                "#95a5a6",
-                "#f39c12",
-                "#d35400",
-                "#c0392b",
-                "#bdc3c7",
-                "#7f8c8d",
-                "#FFF",
-                "#005a66",
-                "#213042",
-                "#0094ab"
-            ], {
+            var pk = new Piklor(".color-picker" + colorbtn, composer.activeModel().parameters.colors, {
                 open: ".picker-wrapper .colorbtn" + colorbtn,
                 closeOnBlur: true
             })
