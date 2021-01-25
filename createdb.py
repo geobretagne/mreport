524eaf6d6dc5a0a40c338df8f9ba6e3754fad881
spelhate <sebastien.pelhate@bretagne.bzh>
2021-01-25 17:23:21 +0100

    Remove data import in createdb.py

--- createdb.py
+++ createdb.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 from backend import db
-from sqlalchemy.sql import text
 
 db.create_all()
-
-with open('backend/datainit/demo.sql') as f:
-  #sql = f.read().replace(':schema', app.config['SCHEMA'])
-  sql = f.read()
-  db.engine.execute(text(sql))
-
-
 print("Tables created in database")
