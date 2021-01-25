b58cb8b861ad73bea3057712833b38a9b7d1f694
spelhate <sebastien.pelhate@bretagne.bzh>
2021-01-25 16:24:55 +0100

    Replace old sql files by demo sql

--- backend/datainit/dataid.sql
+++
@@ -1,2 +0,0 @@
-insert into schema.dataid (dataid, label) values
-('ec_123', 'Ecluse de la malouinière');
--- backend/datainit/report.sql
+++
@@ -1,3 +0,0 @@
-insert into schema.report (report, title) values
-('rapportsurlescluses', 'Rapport sur les écluses'),
-('rapportsurlesponeys', 'Rapport sur les poneys');
