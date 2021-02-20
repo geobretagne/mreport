42562d22dfe332f0ec20dd8eb820ab3c8415f2e8
spelhate <sebastien.pelhate@bretagne.bzh>
2021-02-20 14:19:06 +0100

    Update scripts to set reportids in commande line

---
+++ scripts/export.md
@@ -0,0 +1,24 @@
+# Création d'un fichier demo.sql contenant la structure de la base de données plus les données associées aux rapports sélectionnés.
+
+Le script est à exécuter depuis la machine contenant la base de données MREPORT. Il requiert la présence du fichier export.sql.
+
+## Usage
+
+Des variables sont éventuellement à modifier dans le fichier export.sh
+
+``
+PORT=5432
+SCHEMA=data
+DBSOURCE=dataviz
+DBTEMP=dataviz_demo
+WKS=/tmp
+USER=dataviz_user
+``
+
+`su postgres`
+`./export.sh rapport1 rapport2 rapportn`
+
+Le script produit un fichier demo.sql en sortie avec 2 variables (:schema & :user)
+
+
+
--- scripts/export.sh
+++ scripts/export.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
-
-PORT=5434
+PORT=5432
 SCHEMA=data
 DBSOURCE=dataviz
 DBTEMP=dataviz_demo
 WKS=/tmp
-USER1=dataviz_user
-USER2=dataviz_admin
+USER=dataviz_user
+
+
+reports=()
+for arg; do
+   reports+=($arg)
+done
+
+expr=$(printf ",\'%s\'" "${reports[@]}")
+expr=${expr:1}
+
+echo "Export des rapports $expr"
 
 cd $WKS
 
 # 1 - Create text files with copy command to extract selected data reports 
-psql -p $PORT -d $DBSOURCE -f $WKS/export.sql
+psql -p $PORT -d $DBSOURCE -f $WKS/export.sql --set "expr=$expr"
 
 # 2 dump database tructure only
 pg_dump -f $WKS/dump.sql -p $PORT -d $DBSOURCE --schema-only -n $SCHEMA
 
 #Création base démo
-createdb -p $PORT -O $USER1 $DBTEMP -T template0 -E 'UTF8'
+createdb -p $PORT -O $USER $DBTEMP -T template0 -E 'UTF8'
 
 # import dump in db
 psql -p $PORT -d $DBTEMP -f $WKS/dump.sql
@@ -34,8 +43,7 @@ psql -p $PORT -d $DBTEMP -c "\copy data.report_composition FROM '$WKS/report_com
 pg_dump -f $WKS/demo.sql -p $PORT -d $DBTEMP -n data
 
 # Replace dump user by dataviz user of your installation
-sed -i "s/$USER1/:user/g" $WKS/demo.sql
-sed -i "s/$USER2/:user/g" $WKS/demo.sql
+sed -i "s/$USER/:user/g" $WKS/demo.sql
 
 # Replace dump schema by schema of your installation
 
