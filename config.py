9edce65e1da01ff9ad628457dc751d0cf424dee6
HWFord <lilypickle@hotmail.fr>
2021-03-04 10:03:37 +0100

    ligne manquant config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+DEBUG = True
 API_LOCATION = '/api'
 APP_SCHEME = 'http'
 SQLALCHEMY_DATABASE_URI = 'postgresql://mreport:password@localhost:5432/dataviz'
@@ -5,3 +6,4 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 SCHEMA = "data" ## Comment this line if you don't need schema or if you use SQLite
 MREPORT_REPORTS = "backend/reports"
 MREPORT_LOCATION = "/mreport"
+ADMIN_LOCATION = "/admin"
