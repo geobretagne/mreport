b45a91705874183e14ed547e92dc3549ed75b2f0
HWFord <lilypickle@hotmail.fr>
2021-05-12 11:08:45 +0200

    unit test -- api level

--- backend/tests/unit/test_api_level_get.py
+++ backend/tests/unit/test_api_level_get.py
@@ -6,26 +6,26 @@ import pytest
 
 from flask import app
 
-def test_tu_001():
+def test_api_level_001():
      response = requests.get("http://localhost:5000/api/level/")
      assert response.status_code == 200
 
-def test_tu_002():
+def test_api_level_002():
      response = requests.get("http://localhost:5000/api/level/")
      assert response.headers["Content-Type"] == "application/json"
 
-def test_tu_003():
+def test_api_level_003():
      response = requests.get("http://localhost:5000/api/level/")
      response_body = response.json()
      assert response_body["response"] == "success"
 
-def test_tu_004():
+def test_api_level_004():
      response = requests.get("http://localhost:5000/api/level/")
      response_body = response.json()
-     assert response_body["levels"][0] == "ecluse\n"
-     assert response_body["levels"][1] == "epci" 
+     assert response_body["levels"][0] == "epci"
+     assert response_body["levels"][1] == "ecluse" 
 
-def test_tu_005():
+def test_api_level_005():
      response = requests.get("http://localhost:5000/api/level/")
      response_body = response.json()
      assert len(response_body["levels"]) == 2
