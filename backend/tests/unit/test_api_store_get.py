07ef3e68a84dd9e547941fbb2d3ac52aff6da030
HWFord <lilypickle@hotmail.fr>
2021-05-12 11:09:09 +0200

    unit test -- api store

--- backend/tests/unit/test_api_store_get.py
+++ backend/tests/unit/test_api_store_get.py
@@ -6,15 +6,15 @@ import pytest
 
 from flask import app
 
-def test_get_levels_check_status_code_equals_200():
+def test_api_store_001():
      response = requests.get("http://localhost:5000/api/store/")
      assert response.status_code == 200
 
-def test_get_locations_for_us_90210_check_content_type_equals_json():
+def test_api_store_002():
      response = requests.get("http://localhost:5000/api/store/")
      assert response.headers["Content-Type"] == "application/json"
 
-def test_get_levels_check_response_equals_sucess():
+def test_api_store_003():
      response = requests.get("http://localhost:5000/api/store/")
      response_body = response.json()
      assert response_body["response"] == "success"
