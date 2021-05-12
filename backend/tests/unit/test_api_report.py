4ef5861d2843629caf47dd81cbdc91da588efc59
HWFord <lilypickle@hotmail.fr>
2021-05-12 13:39:27 +0200

    unit tests -- rename tests for api report

--- backend/tests/unit/test_api_report.py
+++ backend/tests/unit/test_api_report.py
@@ -6,15 +6,15 @@ import pytest
 
 from flask import app
 
-def test_api_store_001():
+def test_api_report_001():
      response = requests.get("http://localhost:5000/api/report/")
      assert response.status_code == 200
 
-def test_api_store_002():
+def test_api_report_002():
      response = requests.get("http://localhost:5000/api/report/")
      assert response.headers["Content-Type"] == "application/json"
 
-def test_api_store_003():
+def test_api_report_003():
      response = requests.get("http://localhost:5000/api/report/")
      response_body = response.json()
      assert response_body["response"] == "success"
