ddb7e664214787ab1662c44ea5b83df3000e9006
HWFord <lilypickle@hotmail.fr>
2021-05-12 11:08:57 +0200

    unit test -- api report

---
+++ backend/tests/unit/test_api_report.py
@@ -0,0 +1,20 @@
+import os
+import tempfile
+import requests
+
+import pytest
+
+from flask import app
+
+def test_api_store_001():
+     response = requests.get("http://localhost:5000/api/report/")
+     assert response.status_code == 200
+
+def test_api_store_002():
+     response = requests.get("http://localhost:5000/api/report/")
+     assert response.headers["Content-Type"] == "application/json"
+
+def test_api_store_003():
+     response = requests.get("http://localhost:5000/api/report/")
+     response_body = response.json()
+     assert response_body["response"] == "success"
